73d1e3d854bfe474eb29bfb8493bf229dc978f9a
Akmal Ramadhan <85979442+Akmal76@users.noreply.github.com>
2021-09-16 22:56:05 +0700

    Update 2017_C2_HubunganKakekCucu.cpp

--- 2017_C2_HubunganKakekCucu.cpp
+++ 2017_C2_HubunganKakekCucu.cpp
@@ -2,15 +2,7 @@
 /*OSP 2017 - C2. Hubungan Kakek Cucu*/
 /*Linear Search*/
 #include <bits/stdc++.h>
-#define ll long long
 #define fastio ios_base::sync_with_stdio(false); cin.tie(NULL);
-#define pll pair<ll, ll>
-#define plll pair<ll, pll>
-#define mod 1000000007
-#define fi first
-#define se second
-#define mp make_pair
-#define pb push_back
 using namespace std;
 
 int N, A[100100], banyakhubungan = 0;
